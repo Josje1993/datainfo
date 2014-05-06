a89a2defea7284bf83ad647a257945937c5f13d5
Josje1993 <josje-vt-padje@hotmail.com>
2014-05-06 15:38:30 +0200

    Opdracht 2 task a

---
+++ session2/result2.sql
@@ -0,0 +1,3 @@
+INSERT INTO F(x, y, violation, result) VALUES ('ABC','EF', 0, 'Als je ABC weet, weet je E en F en als je E weet weet je D dus weet je alles dus is ABC een superkey deze schend dus niet de BCNF-conditie voor R');
+INSERT INTO F(x, y, violation, result) VALUES ('E','E', 1, 'Als je E weet, weet je alleen D en A, maar de rest niet dus is dit geen key en schend deze dus wel de BCNF-conditie voor R');
+INSERT INTO F(x, y, violation, result) VALUES ('D','A', 1, 'Als je D weet weet je alleen A en de rest niet dus schend deze de BCNF-conditie voor R');
