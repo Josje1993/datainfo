8dd4dd8a63fd6ae44442dccdafd5127d07f76185
Josje1993 <josje-vt-padje@hotmail.com>
2014-05-06 16:33:06 +0200

    Nog een keer Shuntingrule

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,7 +1,6 @@
-SELECT p.pid FROM Person p 
-WHERE EXISTS( 
-	SELECT * FROM Acts a,  Movie m
-	WHERE a.pid = p.pid
-	AND m.name = 'Back to the Future'
-); 
+SELECT DISTINCT p.pis From Person p, Acts a, Movie m
+WHERE a.pid = p.pid
+AND m.mid = a.mid
+AND m.name = 'Back to the Future'
+; 
 
