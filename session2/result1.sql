f179e368b59e538fdb67999c1d396b30f70e03ec
Josje1993 <josje-vt-padje@hotmail.com>
2014-05-06 16:44:09 +0200

    no message

--- session2/result1.sql
+++ session2/result1.sql
@@ -1,10 +1,10 @@
-INSERT INTO Problem1 (task, result, motivation) VALUES('a', 0, 'meerdere autos kunnen de zelfde capaciteit hebben')
-INSERT INTO Problem1 (task, result, motivation) VALUES('b', 1, 'een auto heeft 1 capaciteit')
-INSERT INTO Problem1 (task, result, motivation) VALUES('c', 1, 'bij het afleveradres en de factuur samen hoort 1 pakketje')
-INSERT INTO Problem1 (task, result, motivation) VALUES('d', 1, 'voor een pakketje is een afleveradres en een factuur')
-INSERT INTO Problem1 (task, result, motivation) VALUES('e', 1, 'voor een pakketje is een geldbedrag en 1 ontvanger')
-INSERT INTO Problem1 (task, result, motivation) VALUES('f', 0, 'een bezorger kan meerdere voertuigen hebben')
-INSERT INTO Problem1 (task, result, motivation) VALUES('g', 1, 'per voertuig is een gezorger gerechtigd')
-INSERT INTO Problem1 (task, result, motivation) VALUES('h', 1, 'een voertuig heeft 1 of meer bestuurders')
-INSERT INTO Problem1 (task, result, motivation) VALUES('i', 1, 'een voertuig heeft 1 of meer capaciteiten')
-INSERT INTO Problem1 (task, result, motivation) VALUES('j', 1, 'een voertuig heeft een of meer bezorgers en een of meer capaciteiten')
+INSERT INTO Problem1 (task, result, motivation) VALUES('a', 0, 'meerdere autos kunnen de zelfde capaciteit hebben');
+INSERT INTO Problem1 (task, result, motivation) VALUES('b', 1, 'een auto heeft 1 capaciteit');
+INSERT INTO Problem1 (task, result, motivation) VALUES('c', 1, 'bij het afleveradres en de factuur samen hoort 1 pakketje');
+INSERT INTO Problem1 (task, result, motivation) VALUES('d', 1, 'voor een pakketje is een afleveradres en een factuur');
+INSERT INTO Problem1 (task, result, motivation) VALUES('e', 1, 'voor een pakketje is een geldbedrag en 1 ontvanger');
+INSERT INTO Problem1 (task, result, motivation) VALUES('f', 0, 'een bezorger kan meerdere voertuigen hebben');
+INSERT INTO Problem1 (task, result, motivation) VALUES('g', 1, 'per voertuig is een gezorger gerechtigd');
+INSERT INTO Problem1 (task, result, motivation) VALUES('h', 1, 'een voertuig heeft 1 of meer bestuurders');
+INSERT INTO Problem1 (task, result, motivation) VALUES('i', 1, 'een voertuig heeft 1 of meer capaciteiten');
+INSERT INTO Problem1 (task, result, motivation) VALUES('j', 1, 'een voertuig heeft een of meer bezorgers en een of meer capaciteiten');
